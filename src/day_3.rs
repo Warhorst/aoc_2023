bb10987dd038901b974990a195b83ee458c76235
Warhorst <46809804+Warhorst@users.noreply.github.com>
2023-12-02 08:15:18 +0100

    Prepared for day 3

---
+++ src/day_3.rs
@@ -0,0 +1,7 @@
+pub fn solve_3a(input: &str) -> usize {
+    0
+}
+
+pub fn solve_3b(input: &str) -> usize {
+    0
+}
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,23 @@ use std::fmt::Debug;
 
 use crate::day_1::{solve_1a, solve_1b};
 use crate::day_2::{solve_2a, solve_2b};
+use crate::day_3::{solve_3a, solve_3b};
 use crate::input::load_input;
 
 mod input;
 mod day_1;
 mod day_2;
+mod day_3;
 
 fn main() {
-    solve_day(2)
+    solve_day(3)
 }
 
 fn solve_day(day: usize) {
     let solve_day_funcs = [
         || solve(1, solve_1a, 142, solve_1b, 281),
         || solve(2, solve_2a, 8, solve_2b, 2286),
+        || solve(3, solve_3a, 0, solve_3b, 0),
     ];
 
     solve_day_funcs[day - 1]()
