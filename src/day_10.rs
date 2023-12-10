de1ed07a0cbaf0352a88465d1eb8da216fa5e265
Warhorst <46809804+Warhorst@users.noreply.github.com>
2023-12-10 12:59:25 +0100

    Solved day 10a

--- src/day_10.rs
+++ src/day_10.rs
@@ -1,7 +1,197 @@
+use std::collections::HashMap;
+use pad::{Direction, p, Position};
+use pad::Direction::*;
+use polygonical::point::Point;
+use polygonical::polygon::Polygon;
+use crate::day_10::Tile::{Ground, Horizontal, NorthEast, NorthWest, SouthEast, SouthWest, Start, Vertical};
+
 pub fn solve_10a(input: &str) -> usize {
-    0
+    let tile_map = input
+        .lines()
+        .enumerate()
+        .flat_map(|(i, line)| line.chars().enumerate().map(move |(j, c)| (p!(j, i), Tile::from(c))))
+        .collect::<HashMap<_, _>>();
+
+    let start_pos = tile_map.iter().filter_map(|(pos, tile)| if tile == &Start {
+        Some(*pos)
+    } else {
+        None
+    }).next().unwrap();
+
+    let width = input.lines().next().unwrap().len();
+    let height = input.lines().count();
+
+    let (mut current_pos, mut current_direction) = start_pos
+        .cardinal_neighbours_with_directions()
+        .into_iter()
+        .filter(|(pos, _)| position_in_bounds(*pos, width, height))
+        .filter(|(pos, dir)| tile_map.get(&pos).unwrap().next_direction(*dir).is_some())
+        .next().unwrap();
+
+    let mut count = 1;
+
+    while tile_map.get(&current_pos).unwrap() != &Start {
+        (current_pos, current_direction) = current_pos
+            .cardinal_neighbours_with_directions()
+            .into_iter()
+            .filter(|(pos, _)| position_in_bounds(*pos, width, height))
+            .filter(|(_, dir)| tile_map.get(&current_pos).unwrap().possible_directions().contains(dir))
+            .filter(|(_, dir)| reverse(dir) != current_direction)
+            .filter(|(pos, dir)| tile_map.get(&pos).unwrap().next_direction(*dir).is_some())
+            .next().unwrap();
+
+        count += 1
+    }
+
+    count / 2
 }
 
 pub fn solve_10b(input: &str) -> usize {
-    0
+    let tile_map = input
+        .lines()
+        .enumerate()
+        .flat_map(|(i, line)| line.chars().enumerate().map(move |(j, c)| (p!(j, i), Tile::from(c))))
+        .collect::<HashMap<_, _>>();
+
+    let start_pos = tile_map.iter().filter_map(|(pos, tile)| if tile == &Start {
+        Some(*pos)
+    } else {
+        None
+    }).next().unwrap();
+
+    let width = input.lines().next().unwrap().len();
+    let height = input.lines().count();
+
+    let mut loop_positions = vec![];
+
+    let (mut current_pos, mut current_direction) = start_pos
+        .cardinal_neighbours_with_directions()
+        .into_iter()
+        .filter(|(pos, _)| position_in_bounds(*pos, width, height))
+        .filter(|(pos, dir)| tile_map.get(&pos).unwrap().next_direction(*dir).is_some())
+        .next().unwrap();
+
+    loop_positions.push(current_pos);
+
+    while tile_map.get(&current_pos).unwrap() != &Start {
+        (current_pos, current_direction) = current_pos
+            .cardinal_neighbours_with_directions()
+            .into_iter()
+            .filter(|(pos, _)| position_in_bounds(*pos, width, height))
+            .filter(|(_, dir)| tile_map.get(&current_pos).unwrap().possible_directions().contains(dir))
+            .filter(|(_, dir)| reverse(dir) != current_direction)
+            .filter(|(pos, dir)| tile_map.get(&pos).unwrap().next_direction(*dir).is_some())
+            .next().unwrap();
+
+        loop_positions.push(current_pos);
+    }
+
+    let polygon = Polygon::new(
+        loop_positions
+            .iter()
+            .inspect(|pos| println!("{:?}", pos))
+            .map(|pos| Point::new(pos.x as f32, pos.y as f32))
+            .collect()
+    );
+
+    // Always returns 0...
+    tile_map
+        .keys()
+        .filter(|pos| !loop_positions.contains(pos))
+        .filter(|pos| polygon.contains(Point::new(pos.x as f32, pos.y as f32)))
+        .count()
+}
+
+fn position_in_bounds(pos: Position, width: usize, height: usize) -> bool {
+    pos.x >= 0 && pos.x < width as isize && pos.y >= 0 && pos.y < height as isize
+}
+
+fn reverse(dir: &Direction) -> Direction {
+    match *dir {
+        XP => XM,
+        XM => XP,
+        YP => YM,
+        YM => YP,
+        _ => panic!("invalid dir")
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum Tile {
+    Vertical,
+    Horizontal,
+    NorthEast,
+    NorthWest,
+    SouthWest,
+    SouthEast,
+    Ground,
+    Start,
+}
+
+impl Tile {
+    fn next_direction(&self, goiing: Direction) -> Option<Direction> {
+        match self {
+            Vertical => match goiing {
+                YP => Some(YP),
+                YM => Some(YM),
+                _ => None
+            }
+            Horizontal => match goiing {
+                XP => Some(XP),
+                XM => Some(XM),
+                _ => None
+            }
+            NorthEast => match goiing {
+                YP => Some(XP),
+                XM => Some(YM),
+                _ => None
+            }
+            NorthWest => match goiing {
+                XP => Some(YM),
+                YP => Some(XM),
+                _ => None
+            }
+            SouthWest => match goiing {
+                YM => Some(XM),
+                XP => Some(YP),
+                _ => None
+            }
+            SouthEast => match goiing {
+                YM => Some(XP),
+                XM => Some(YP),
+                _ => None
+            }
+            Ground => None,
+            Start => Some(XP),
+        }
+    }
+
+    fn possible_directions(&self) -> Vec<Direction> {
+        match self {
+            Vertical => vec![YP, YM],
+            Horizontal => vec![XP, XM],
+            NorthEast => vec![YM, XP],
+            NorthWest => vec![YM, XM],
+            SouthWest => vec![YP, XM],
+            SouthEast => vec![YP, XP],
+            Ground => vec![],
+            Start => vec![XP, XM, YP, YM]
+        }
+    }
+}
+
+impl From<char> for Tile {
+    fn from(value: char) -> Self {
+        match value {
+            '|' => Vertical,
+            '-' => Horizontal,
+            'L' => NorthEast,
+            'J' => NorthWest,
+            '7' => SouthWest,
+            'F' => SouthEast,
+            '.' => Ground,
+            'S' => Start,
+            c => panic!("unknown char {c}")
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ fn solve_day(day: usize) {
         || solve(7, solve_7a, 6440, solve_7b, 5905),
         || solve(8, solve_8a, 6, solve_8b, 6),
         || solve(9, solve_9a, 114, solve_9b, 2),
-        || solve(10, solve_10a, 0, solve_10b, 0),
+        || solve(10, solve_10a, 8, solve_10b, 10),
     ];
 
     solve_day_funcs[day - 1]()
